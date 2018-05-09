594c9c24403f73ff315ca5a4fe85ccb1a22ea0ca
nokranger <anusorn.thavornporn@gmail.com>
2018-05-09 14:22:50 +0700

    Add files via upload

---
+++ getTAG.ino
@@ -0,0 +1,75 @@
+/* getTAG.ino
+ *
+ * Created by Danilo Queiroz Barbosa, August 26, 2016.
+ * Member of electronicdrops.com
+ *
+ * This example shows how to use the RFIDRdm630 library.
+ * When a card is read, an ASCII TAG and a Card Number
+ * will be show in the Serial.*
+ *
+ *
+ *   This example code is in the public domain.
+
+ Note:
+ Not all pins on the Mega and Mega 2560 support change interrupts,
+ so only the following can be used for RX:
+ 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
+
+ */
+
+#include "RFIDRdm630mod.h"
+//char * tag;
+
+RFIDtag  in_tag;  // RFIDtag object
+RFIDtag  out_tag;  // RFIDtag object
+
+RFIDRdm630 in_reader = RFIDRdm630(&Serial1);    // the reader object.
+RFIDRdm630 out_reader = RFIDRdm630(&Serial2);    // the reader object.
+
+int updates;
+int failedUpdates;
+int pos;
+int stringplace = 0;
+
+String latitude = "";
+String longitude = "";
+
+String timeUp;
+String nmea[15];
+
+void setup() {
+  count=0;
+  Serial.begin(9600);   // open the Serial for show data
+  gps_setup();
+    pinMode(ledPin, OUTPUT);
+}
+void loop() {
+  if (in_reader.isAvailable()){  // tests if a card was read by the module
+    
+    in_tag = in_reader.getTag();  // if true, then receives a tag object
+    Serial.print("IN Card Number: ");
+    Serial.print(in_tag.getCardNumber());  // get cardNumber in long format
+
+    gps_loop();
+    
+    Serial.print(" GPS Latitude: ");
+    Serial.print(latitude);  // get cardNumber in long format
+    Serial.print(" GPS Longitude: ");
+    Serial.print(longitude);  // get cardNumber in long format
+  }
+
+  if (out_reader.isAvailable()){  // tests if a card was read by the module
+
+    out_tag = out_reader.getTag();  // if true, then receives a tag object
+    Serial.print("OUT Card Number: ");
+    Serial.print(out_tag.getCardNumber());  // get cardNumber in long format
+    
+    gps_loop();
+    
+    Serial.print(" GPS Latitude: ");
+    Serial.print(latitude);  // get cardNumber in long format
+    Serial.print(" GPS Longitude: ");
+    Serial.print(longitude);  // get cardNumber in long format    
+  }
+
+}
---
+++ gsp.ino
@@ -0,0 +1,96 @@
+void gps_setup() {
+  Serial3.begin(9600);
+}
+
+void gps_loop() {
+  Serial3.flush();
+  while (Serial3.available() > 0)
+  {
+    Serial3.read();
+
+  }
+  if (Serial3.find("$GPRMC,")) {
+    String tempMsg = Serial3.readStringUntil('\n');
+    for (int i = 0; i < tempMsg.length(); i++) {
+      if (tempMsg.substring(i, i + 1) == ",") {
+        nmea[pos] = tempMsg.substring(stringplace, i);
+        stringplace = i + 1;
+        pos++;
+      }
+      if (i == tempMsg.length() - 1) {
+        nmea[pos] = tempMsg.substring(stringplace, i);
+      }
+    }
+    updates++;
+    nmea[2] = ConvertLat();
+    nmea[4] = ConvertLng();
+
+    latitude = nmea[2];
+    longitude = nmea[4];
+
+  }
+  else {
+
+    failedUpdates++;
+
+  }
+  stringplace = 0;
+  pos = 0;
+}
+
+String ConvertLat() {
+  String posneg = "";
+  if (nmea[3] == "S") {
+    posneg = "-";
+  }
+  String latfirst;
+  float latsecond;
+  for (int i = 0; i < nmea[2].length(); i++) {
+    if (nmea[2].substring(i, i + 1) == ".") {
+      latfirst = nmea[2].substring(0, i - 2);
+      latsecond = nmea[2].substring(i - 2).toFloat();
+    }
+  }
+  latsecond = latsecond / 60;
+  String CalcLat = "";
+
+  char charVal[9];
+  dtostrf(latsecond, 4, 6, charVal);
+  for (int i = 0; i < sizeof(charVal); i++)
+  {
+    CalcLat += charVal[i];
+  }
+  latfirst += CalcLat.substring(1);
+  latfirst = posneg += latfirst;
+  return latfirst;
+}
+
+String ConvertLng() {
+  String posneg = "";
+  if (nmea[5] == "W") {
+    posneg = "-";
+  }
+
+  String lngfirst;
+  float lngsecond;
+  for (int i = 0; i < nmea[4].length(); i++) {
+    if (nmea[4].substring(i, i + 1) == ".") {
+      lngfirst = nmea[4].substring(0, i - 2);
+      //Serial.println(lngfirst);
+      lngsecond = nmea[4].substring(i - 2).toFloat();
+      //Serial.println(lngsecond);
+
+    }
+  }
+  lngsecond = lngsecond / 60;
+  String CalcLng = "";
+  char charVal[9];
+  dtostrf(lngsecond, 4, 6, charVal);
+  for (int i = 0; i < sizeof(charVal); i++)
+  {
+    CalcLng += charVal[i];
+  }
+  lngfirst += CalcLng.substring(1);
+  lngfirst = posneg += lngfirst;
+  return lngfirst;
+}
